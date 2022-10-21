e637171aa494a19d2b2b7ced11f9aac46770ebda
Jonathan Macedo <jonathan.silvamacedo@gmail.com>
2022-10-21 09:13:26 -0300

    Adding a typeWriter function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-$(".name").on("mouseover", function() {
-  $(".name").css("color", "blue");
-})
+var i = 0;
+var txt = "Hello, I'm a Chemist, a Data Science student and I work as a Laboratory Analyst at PróLab Biotechnology.";
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typed-text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+$(".about").click(function () {
+  typeWriter();
+});
