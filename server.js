5e9b47360a0b1552475696fbba8cf7fba591820d
jmacedo91 <jonathan.silvamacedo@gmail.com>
2022-11-17 22:32:34 -0300

    Começando o backend

---
+++ server.js
@@ -0,0 +1,20 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+// app.post("/", function(req, res) {
+//
+// })
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
